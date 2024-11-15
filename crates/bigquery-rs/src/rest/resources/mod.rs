c1d166e49e448ff61c16ef918a28e7a07f10aaeb
mrudisel <mitchrudisel@gmail.com>
2024-11-15 11:52:24 -0800

    fix optional field

--- crates/bigquery-rs/src/rest/resources/mod.rs
+++ crates/bigquery-rs/src/rest/resources/mod.rs
@@ -16,7 +16,8 @@ pub struct TableReference<S = Box<str>> {
 #[error("{message}: {reason}")]
 pub struct ErrorProto<S = Box<str>> {
     pub reason: S,
-    pub location: S,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub location: Option<S>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub debug_info: Option<S>,
     pub message: S,
