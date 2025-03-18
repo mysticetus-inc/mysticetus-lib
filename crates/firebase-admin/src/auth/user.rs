3278ec7659f3eb287045cbb23e63f4b2b096c26e
mrudisel <mitchrudisel@gmail.com>
2025-03-17 17:47:06 -0700

    fill out user fields

--- crates/firebase-admin/src/auth/user.rs
+++ crates/firebase-admin/src/auth/user.rs
@@ -1,7 +1,48 @@
-use std::collections::BTreeMap;
+use std::sync::Arc;
+
+use timestamp::Timestamp;
 
 #[derive(Debug, Clone, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct UserInfo {
-    #[serde(flatten)]
-    inner: BTreeMap<Box<str>, serde_json::Value>,
+    created_at: Timestamp,
+    #[serde(default)]
+    disabled: bool,
+    email: Arc<str>,
+    #[serde(alias = "localId")]
+    uid: Arc<str>,
+    valid_since: Option<Timestamp>,
+    photo_url: Option<Box<str>>,
+    last_login_at: Option<Timestamp>,
+    display_name: Option<Box<str>>,
+}
+
+impl UserInfo {
+    pub fn uid(&self) -> &Arc<str> {
+        &self.uid
+    }
+
+    pub fn email(&self) -> &Arc<str> {
+        &self.email
+    }
+
+    pub fn created_at(&self) -> Timestamp {
+        self.created_at
+    }
+
+    pub fn disabled(&self) -> bool {
+        self.disabled
+    }
+
+    pub fn display_name(&self) -> Option<&str> {
+        self.display_name.as_deref()
+    }
+
+    pub fn photo_url(&self) -> Option<&str> {
+        self.photo_url.as_deref()
+    }
+
+    pub fn last_login_at(&self) -> Option<Timestamp> {
+        self.last_login_at
+    }
 }
